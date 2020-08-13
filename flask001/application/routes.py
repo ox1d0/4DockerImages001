3d932fc554a06037cbdd5bbced82a0712807374e
ox1d02 <ox1d0.a@gmail.com>
2020-08-13 00:09:06 -0500

    Update routes.py

--- flask001/application/routes.py
+++ flask001/application/routes.py
@@ -74,4 +74,4 @@ def user(name):
         return '<h1>Hello, {}!</h1>'.format(name)
     
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=4567, debug=False)
+        app.run(host="0.0.0.0", port=5000, debug=False)
