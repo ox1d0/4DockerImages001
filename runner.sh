4de29afd404795928993c9f2924ef659a9fa8dcc
ox1d02 <ox1d0.a@gmail.com>
2020-08-13 11:16:13 -0500

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-uwsgi --http-socket :5000 --plugin python --wsgi-file app.wsgi 
+uwsgi --http-socket :5000 --plugin python --wsgi-file app.wsgi &
+printf "uwsgi Done\n"
 # Startup the cmd
+nginx &
